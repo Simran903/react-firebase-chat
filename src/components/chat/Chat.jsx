354c1546986d3face858e12f75c4a73d28fc6c3f
Simran <itssimran903@gmail.com>
2024-05-02 01:14:19 +0530

    commit

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,95 +1,130 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './chat.css'
-import EmojiPicker from 'emoji-picker-react'
-import { AiOutlineBgColors } from 'react-icons/ai';
-import useUserStore from '../../lib/userStore';
-import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
-import { db } from '../../lib/firebase';
-import useChatStore from '../../lib/chatStore';
-
+import React, { useEffect, useRef, useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+import { AiOutlineBgColors } from "react-icons/ai";
+import useUserStore from "../../lib/userStore";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../../lib/firebase";
+import useChatStore from "../../lib/chatStore";
+import upload from "../../lib/upload";
 
 const Chat = () => {
-  const [chat, setChat] = useState()
-  const [open, setOpen] = useState(false)
-  const [text, setText] = useState('')
-  const { color, setColor } = useUserStore();
-  const { chatId, user } = useChatStore()
-  const { currentUser } = useUserStore()
+  const [chat, setChat] = useState();
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+  const [img, setImg] = useState({
+    file: null,
+    url: "",
+  });
+  const { chatId, user, isCurrentUserBlocked, isReceiverBlocked } = useChatStore();
+  const { currentUser } = useUserStore();
 
-  const endRef = useRef(null)
+  const endRef = useRef(null);
 
   useEffect(() => {
-    endRef.current?.scrollIntoView({behavior: 'smooth'})
-  }, [])
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+
+    const savedColor = localStorage.getItem("chatColor");
+    if (savedColor) {
+      setColor(savedColor);
+    }
+  }, []);
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
-      setChat(res.data())
-    } )
-  
-    return () => {
-      unSub()
-    }
-  }, [chatId])
+      setChat(res.data());
+    });
 
-  console.log(chat)
-  
+    return () => {
+      unSub();
+    };
+  }, [chatId]);
+
+  const { color, setColor } = useUserStore();
 
   const handleEmoji = (e) => {
-    setText((prev) => prev + e.emoji)
-    setOpen(false)
-  }
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  const handleImage = (e) => {
+    if (e.target.files[0]) {
+      setImg({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
 
   const handleColorChange = (e) => {
     setColor(e.target.value);
-  }
+    localStorage.setItem("chatColor", e.target.value); // Save color to local storage
+  };
 
   const handleSend = async () => {
     if (text === "") return;
+    let imgUrl = null;
 
     try {
+      if (img.file) {
+        imgUrl = await upload(img.file);
+      }
 
       await updateDoc(doc(db, "chats", chatId), {
-        messages:arrayUnion({
+        messages: arrayUnion({
           senderId: currentUser.id,
           text,
-          createdAt: new Date()
-        })
-      })
-      const userIDs = [currentUser.id, user.id]
+          createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl }),
+        }),
+      });
+      const userIDs = [currentUser.id, user.id];
 
       userIDs.forEach(async (id) => {
+        const userChatsRef = doc(db, "userChats", id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
 
-        const userChatsRef = doc(db, "userChats", id)
-        const userChatsSnapshot = await getDoc(userChatsRef)
-      if (userChatsSnapshot.exists()) {
-        const userChatsData = userChatsSnapshot.data()
-        
-        const chatIndex = userChatsData.chats.findIndex((c) => c.chatId === chatId)
-        
-        userChatsData.chats[chatIndex].lastMessage = text
-        userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true : false
-        userChatsData.chats[chatIndex].updatedAt = Date.now()
-        
-        await updateDoc(userChatsRef, {
-          chats: userChatsData.chats,
-        })
-      }
-    })
-      
+          const chatIndex = userChatsData.chats.findIndex(
+            (c) => c.chatId === chatId
+          );
+
+          userChatsData.chats[chatIndex].lastMessage = text;
+          userChatsData.chats[chatIndex].isSeen =
+            id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatsRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
-  }
+    setImg({
+      file: null,
+      url: "",
+    });
+
+    setText("");
+  };
 
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="top">
         <div className="user">
-          <img src="./avatar.png" alt="" />
+          <img src={user.avatar || "./avatar.png"} alt="" />
           <div className="texts">
-            <span style={{ color: color }}>Jane Doe</span>
+            <span style={{ color: color }}>{user.username}</span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
         </div>
@@ -101,40 +136,79 @@ const Chat = () => {
       </div>
 
       <div className="center">
-        { chat?.messages?.map((message) => (
-        <div className="message own" key={message?.createdAt}>
-          <div className="text">
-            {message?.img && <img src={message?.img} alt="" />}
-            <p style={{ backgroundColor: color }}>{message.text}</p>
-            {/* <span>{message.createdAt}</span> */}
+        {chat?.messages?.map((message) => (
+          <div
+            className={
+              message.senderId === currentUser?.id ? "message own" : "message"
+            }
+            key={message?.createdAt}
+          >
+            <div className="text">
+              {message?.img && <img src={message?.img} alt="" />}
+              <p style={{ backgroundColor: color }}>{message.text}</p>
+              {/* <span>{message.createdAt}</span> */}
+            </div>
           </div>
-        </div>
-        ))
-        }
+        ))}
+        {img.url && (
+          <div className="message own">
+            <div className="text">
+              <img src={img.url} alt="" />
+            </div>
+          </div>
+        )}
+
         <div ref={endRef}></div>
       </div>
 
       <div className="bottom">
         <div className="icons">
-          <img src="./img.png" alt="" />
+          <label htmlFor="file">
+            <img src="./img.png" alt="" />
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleImage}
+          />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder='Type a message...' value={text} onChange={(e) => setText(e.target.value)} />
-        <input type="color" value={color} onChange={handleColorChange} name='color' id='color' className='colorPicker' />
-        <label htmlFor="color"><AiOutlineBgColors className='colorIcon' /></label>
+        <input
+          type="text"
+          placeholder={(isCurrentUserBlocked || isReceiverBlocked) ? "You cannot send a message" : "Type a message..."}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          disabled={isCurrentUserBlocked || isReceiverBlocked}
+        />
+        <input
+          type="color"
+          value={color}
+          onChange={handleColorChange}
+          name="color"
+          id="color"
+          className="colorPicker"
+        />
+        <label htmlFor="color">
+          <AiOutlineBgColors className="colorIcon" />
+        </label>
         <div className="emoji">
-          <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
           <div className="picker">
             <EmojiPicker open={open} onEmojiClick={handleEmoji} />
           </div>
         </div>
-        <button className='sendButton' onClick={handleSend}>
+        <button className="sendButton" onClick={handleSend} disabled={isCurrentUserBlocked || isReceiverBlocked}>
           Send
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Chat;
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './detail.css'
-import { auth } from '../../lib/firebase'
+import { auth, db } from '../../lib/firebase'
 import { useNavigate } from 'react-router-dom';
+import useUserStore from '../../lib/userStore';
+import useChatStore from '../../lib/chatStore';
+import { toast } from 'react-toastify';
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 
 const Detail = () => {
   const navigate = useNavigate();
+  const { currentUser } = useUserStore()
+  const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock } = useChatStore()
 
   const handleLogout = () => {
     auth.signOut()
@@ -12,11 +18,27 @@ const Detail = () => {
       .catch(error => console.error('Error logging out:', error));
   };
 
+  const handleBlock = async () => {
+    if (!user) return;
+    const userDocRef = doc(db, "users", currentUser.id)
+
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id)
+      })
+      toast.success("Blocked successfully")
+      changeBlock()
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
     <div className='detail'>
       <div className="user">
-        <img src="./avatar.png" alt="" />
-        <h2>Jane Doe</h2>
+        <img src={user.avatar || "./avatar.png"} alt="" />
+        <h2>{user.username}</h2>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
       <div className="info">
@@ -69,7 +91,13 @@ const Detail = () => {
             <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-        <button>Block User</button>
+        <button onClick={handleBlock} >
+          {isCurrentUserBlocked
+          ? "You are Blocked"
+          : isReceiverBlocked
+          ? "User Blocked"
+          : "Block User"}
+        </button>
         <button className='logOut' onClick={() => handleLogout()}>Logout</button>
       </div>
     </div>
